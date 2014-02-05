b680495ebf9aea89a2c3aef41332bc6cee65e3c4
Sam Barnes <sam@avantrural.com>
2014-02-05 23:55:13 +0000

    remove confusing promise functionality

--- debounce.js
+++ debounce.js
@@ -1,32 +1,22 @@
+/* global angular */
+
 angular.module('debounce', [])
 
-  // Returns a function, that, as long as it continues to be invoked, will not
-  // be triggered. The function will be called after it stops being called for
-  // N milliseconds. If `immediate` is passed, trigger the function on the
-  // leading edge, instead of the trailing.
-  .factory('debounce', ['$timeout', '$q', function($timeout, $q) {
-    return function(func, wait, immediate) {
+  .factory('debounce', ['$timeout', '$q', function ($timeout, $q) {
+
+    return function (func, wait, immediate) {
       var timeout;
-      var deferred = $q.defer();
-      return function() {
+
+      return function () {
         var context = this, args = arguments;
-        var later = function() {
+        var later = function () {
           timeout = null;
-          if(!immediate) {
-            deferred.resolve(func.apply(context, args));
-            deferred = $q.defer();
-          }
+          if(!immediate) func.apply(context, args);
         };
         var callNow = immediate && !timeout;
-        if (timeout) {
-          $timeout.cancel(timeout);
-        }
+        if (timeout) $timeout.cancel(timeout);
         timeout = $timeout(later, wait);
-        if (callNow) {
-          deferred.resolve(func.apply(context,args));
-          deferred = $q.defer();
-        }
-        return deferred.promise;
+        if (callNow) func.apply(context, args);
       };
     };
   }]);
