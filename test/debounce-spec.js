ac8d65f85a329854e01c4e6955a4816d2d7a43e6
Sam Barnes <sam@avantrural.com>
2014-02-06 01:06:32 +0000

    add tests

---
+++ test/debounce-spec.js
@@ -0,0 +1,57 @@
+
+describe('debounce service', function () {
+  var debounce, $timeout, obj;
+
+  beforeEach(module('debounce'));
+
+  beforeEach(inject(function($injector) {
+    debounce = $injector.get('debounce');
+    $timeout = $injector.get('$timeout');
+  }));
+
+  it('should be a function', function() {
+    expect(typeof debounce).toBe('function');
+  });
+
+  describe('trailing invocation (default)', function () {
+
+    beforeEach(inject(function($injector) {
+      obj = { func: function () {}};
+      spyOn(obj, 'func');
+      obj.wrapped = debounce(obj.func, 10);
+    }));
+
+    it('should work', function() {
+      obj.wrapped(1);
+      $timeout.flush(6);
+      obj.wrapped(2);
+      $timeout.flush(7);
+      obj.wrapped(3);
+      $timeout.flush(15);
+
+      expect(obj.func).toHaveBeenCalledWith(3);
+      expect(obj.func.calls.length).toEqual(1);
+    });
+  });
+
+  describe('leading invocation', function () {
+
+    beforeEach(inject(function($injector) {
+      obj = { func: function () {}};
+      spyOn(obj, 'func');
+      obj.wrapped = debounce(obj.func, 10, true);
+    }));
+
+    it('should work', function() {
+      obj.wrapped(1);
+      $timeout.flush(6);
+      obj.wrapped(2);
+      $timeout.flush(7);
+      obj.wrapped(3);
+      $timeout.flush(15);
+
+      expect(obj.func).toHaveBeenCalledWith(1);
+      expect(obj.func.calls.length).toEqual(1);
+    });
+  });
+});
